77730184d60d08fd1d85bd030e3adb1fffe1ce5b
qipan <qipan@qipandeiMac.local>
2016-12-01 16:26:02 +0800

    add validation before authenticating with server

--- frontend/src/js/views/LoginView.js
+++ frontend/src/js/views/LoginView.js
@@ -1,6 +1,6 @@
 define([
   'js/lib/backbone',
-  'js/lib/text!../../tpl/login.html',
+  'js/lib/text!/tpl/login.html',
 ], function(Backbone, loginTpl) {
     return LoginView = Backbone.View.extend({
       tagName: 'div',
@@ -11,14 +11,50 @@ define([
       },
 
       render: function() {
-        $('#content').html(this.$el.append(this.template({
+        $('#content').empty().html(this.$el.html(this.template({
           systemName: '个人财务管理系统',
         })));
         return this;
       },
 
       login: function(e) {
-//        e.preventDefault();
+        var $loginBtn = $(e.target);
+        var $user = $('#username');
+        var $pass = $('#password');
+
+        e.preventDefault();
+        $loginBtn.addClass('disabled');
+
+        if ($user.val() === '') {
+          $user.addClass('form-error');
+          $('#user_error').text('请输入您的用户名');
+          $loginBtn.removeClass('disabled');
+          $user.focus();
+          return;
+        }
+
+        if ($pass.val() === '') {
+          $pass.addClass('form-error');
+          $('#pass_error').text('请输入您的密码');
+          $loginBtn.removeClass('disabled');
+          $user.focus();
+          return;
+        }
+
+        $.ajax({
+          url: '/api/auth',
+          method: 'POST',
+          data: 'username=' + $username.val() +
+                '&password=' + $username.val(),
+          success: function(data) {
+            if (data.msg == 'Authenticated') {
+              app.navigate('home', {trigger: true});
+            } else {
+              $loginBtn.removeClass('disabled');
+
+            }
+          },
+        });
       },
     });
   });
